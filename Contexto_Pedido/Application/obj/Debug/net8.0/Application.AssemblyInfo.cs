e3ace834ab10db89728c5767e7f6bcd4f6267a48
BrunoBafilli <brunobafillidev@gmail.com>
2024-07-16 17:18:24 -0300

    Update
    Update

--- Contexto_Pedido/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Contexto_Pedido/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd2bceb7802ee6f2ce0f87c16af4deb1b1143b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a560e1bcafdf629b2d8a86cc55076b5ad4f6c913")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
