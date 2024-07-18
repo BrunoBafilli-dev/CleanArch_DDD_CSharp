3cab4d564fd51e478e423b6b247c578c36b099e6
BrunoBafilli <brunobafillidev@gmail.com>
2024-07-18 20:20:45 -0300

    Update
    Update

--- Contexto_Pedido/Console/obj/Debug/net8.0/Console.AssemblyInfo.cs
+++ Contexto_Pedido/Console/obj/Debug/net8.0/Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f9a9c08c6ab86b04cd95f29f0b78fa3a8a09cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4faf7c25734856713ef2650aaf7e3f0d3f64820")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
