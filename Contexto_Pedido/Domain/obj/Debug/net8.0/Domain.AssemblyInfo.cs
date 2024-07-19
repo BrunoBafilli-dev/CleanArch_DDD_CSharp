908f192887924dd69ab7ec00375fa83924ebfde7
BrunoBafilli <brunobafillidev@gmail.com>
2024-07-18 21:15:57 -0300

    Update
    Update

--- Contexto_Pedido/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Contexto_Pedido/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4faf7c25734856713ef2650aaf7e3f0d3f64820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cab4d564fd51e478e423b6b247c578c36b099e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
